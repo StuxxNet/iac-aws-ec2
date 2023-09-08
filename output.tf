6a057c3ed8a27edd09e53a8f7cd553c17fc2dfc8
Ramon Borges <ramonboorges@gmail.com>
2023-09-09 00:02:00 +0200

    feat: Added public ip to the outputs

--- output.tf
+++ output.tf
@@ -3,6 +3,11 @@ output "instance_id" {
   description = "value of the instance id"
 }
 
+output "public_ip" {
+  value       = aws_instance.this.public_ip
+  description = "value of the public ip of the instance"
+}
+
 output "root_disk_size" {
     value       = aws_instance.this.root_block_device.0.volume_size
     description = "value of root the disk size"
